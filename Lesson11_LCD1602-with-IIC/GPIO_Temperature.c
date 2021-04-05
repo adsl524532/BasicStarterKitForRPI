f9e291c1543466a27d3dd7f8bbf240ca3afb9dda
Chris <adsl524532@gmail.com>
2021-04-05 23:03:07 +0200

    Some changes

--- Lesson11_LCD1602-with-IIC/GPIO_Temperature.c
+++ Lesson11_LCD1602-with-IIC/GPIO_Temperature.c
@@ -48,8 +48,13 @@
 #define AF_DB5 (AF_BASE + 5)
 #define AF_DB6 (AF_BASE + 6)
 #define AF_DB7 (AF_BASE + 7)
+
 // Global lcd handle:
 static int lcdHandle;
+
+
+#define myLed_pin  0     // GPIO pin 17
+#define myLed_pin2 2     // GPIO pin 27
 
 // for DHT11 ======================================================
 #define MAXTIMINGS  85
@@ -70,6 +75,7 @@ int ProcessArgs(int argc, char **argv)
 
    printf( "Welcome to Elecrow\n");
    printf( "Raspberry Pi LCD1602 with IIC test program\n\t LCD-address: 0x3F\n\n" );
+   printf( "\tcompiled: %s - %s\n", __DATE__, __TIME__);
    
    printf("number of args: %d\n", argc);
 
@@ -112,10 +118,13 @@ char *PrintTime(void)
 int main(int argc, char**argv)
 {
 	int i;
-	float T, T2;
+	float T, T8, T8_prv=0;
 //	char szIP_address[40];
-        char szT[40], szT2[40];
+        char szT[40], szT8[40];
 	wiringPiSetup();        //Initialise WiringPi
+        pinMode(myLed_pin , OUTPUT);
+        pinMode(myLed_pin2, OUTPUT);
+
 
 	int DisplayAddress = ProcessArgs(argc, argv);
 
@@ -132,7 +141,8 @@ int main(int argc, char**argv)
 	for(i=0;i<8;i++)
 		pinMode(AF_BASE+i,OUTPUT); 	//Will expand the IO port as the output mode
 	
-	digitalWrite(AF_LED,1); 		//Open back light
+//        digitalWrite(AF_LED,1); 		//Open back light
+//      digitalWrite(AF_LED,0); 		//Close back light
 	digitalWrite(AF_RW,0); 			//Set the R/Wall to a low level, LCD for the write state
 	
 	while(1) {
@@ -157,11 +167,26 @@ int main(int argc, char**argv)
 	   lcdPuts(lcdHandle, szTime);
 
 
-           getTemperature(&T, &T2);   	// DS18B20 takes about 1 sec to execute
+           getTemperature(&T, &T8);   	// DS18B20 takes about 1 sec to execute
+           if (T8-T>2.0) {              // T8 radiator temp more then 2 deg higher
+              digitalWrite(myLed_pin , HIGH);
+           } else {
+              digitalWrite(myLed_pin, LOW);
+           }
+           if (T8 > T8_prv+0.10) {         // Radiator temp. is rising
+              T8_prv = T8;
+              digitalWrite(myLed_pin2, HIGH);
+           } else if (T8 < T8_prv-0.0) {
+              T8_prv = T8;
+              digitalWrite(myLed_pin2, LOW);
+           }
+
+
+
            sprintf(szT , "DS18B20: T=%.3f", T);
-           sprintf(szT2, "DS18B20: T=%.3f : %.4f", T2, T - T2);   // show difference
+           sprintf(szT8, "DS18B20: T=%.3f : %.4f", T8, T8 - T);   // show difference
            printf("%s\n",szT);
-           printf("%s\n",szT2);
+           printf("%s\n",szT8);
 
 	   lcdPosition(lcdHandle,0,1);          
 //	   lcdClear(lcdHandle); 	//Clear display
@@ -170,7 +195,7 @@ int main(int argc, char**argv)
            int Error;
            double T2, H2, T3, H3;
 
-	   printf("DHT22: \n");
+	   printf("DHT22  : ");
 	   Error = DHT_Read(DHT22PIN, &T2, &H2, SENSOR_DHT22);		
            if (!Error) {
    	       lcdPosition(lcdHandle,0,2);      // DHT22
@@ -179,8 +204,8 @@ int main(int argc, char**argv)
 		printf("\n");
            }
 
-           sleep(5);
-	   printf("DHT11: \n");
+           sleep(10);
+	   printf("DHT11  : ");
 	   Error = DHT_Read(DHT11PIN, &T3, &H3, SENSOR_DHT11);		
            if (!Error) {
    	       lcdPosition(lcdHandle,0,3);     	// DHT11    
@@ -218,6 +243,8 @@ int main(int argc, char**argv)
            if (fp2) {
               fprintf(fp2,"/usr/bin/curl \"https://api.thingspeak.com/update?api_key=8G8DM5II9O7BC9EG");
               fprintf(fp2,"&field1=%.3f&field2=%.2f&field3=%.2f&field4=%.2f&field5=%.2f&field6=%.2f&field7=%.2f&field8=%.2f\"\n", T, T3, H3, T2, H2, cTemp, humidity, pressure);
+              fprintf(fp2,"/usr/bin/curl \"https://api.thingspeak.com/update?api_key=6RJ7LMZ7V0JJSUK8");
+              fprintf(fp2,"&field1=%.3f\"\n", T8);
               fclose(fp2);
            } else
 	      printf("Cannot open file %s\n", szFilename2);
@@ -278,7 +305,7 @@ float getTemperature(float *T1, float *T2)
    fgets(szString1, 49, fd);
    fgets(szString2, 49, fd);
    fclose(fd);
-   *T1 =atof(szString2+29)/1000;  
+   *T1 =atof(szString2+29)/1000;  // temp is at pos 29
 
 
    fd = fopen("/sys/bus/w1/devices/28-3c01d607b280/w1_slave", "r");
@@ -342,7 +369,8 @@ int DHT_Read(int DhtPin, double *T, double *H, int SensorType)
         {
             /* shove each bit into the storage bytes */
             dht11_dat[j / 8] <<= 1;
-            if ( counter > 16 )
+    // original        if ( counter > 16 )
+            if ( counter > 50 )
                 dht11_dat[j / 8] |= 1;
             j++;
         }
@@ -368,8 +396,8 @@ int DHT_Read(int DhtPin, double *T, double *H, int SensorType)
          *H = dht11_dat[0] + dht11_dat[1]/10.;
          *T = dht11_dat[2] + dht11_dat[3]/10.;
 
-         printf("DHT11 Sux T = %d.%d, H = %d.%d\n", dht11_dat[2], dht11_dat[3], dht11_dat[0], dht11_dat[1]);
-         printf("DHT11 Sub:T = %5.2f, H = %5.2f\n", *T, *H);
+//       printf("DHT11 Sux T = %d.%d, H = %d.%d\n", dht11_dat[2], dht11_dat[3], dht11_dat[0], dht11_dat[1]);
+//         printf("Sub:T = %5.2f, H = %5.2f\n", *T, *H);
 
          message1[ 7] = dht11_dat[2] / 10 + 48;
          message1[ 8] = dht11_dat[2] % 10 + 48;
@@ -391,7 +419,7 @@ int DHT_Read(int DhtPin, double *T, double *H, int SensorType)
          *T = Temp/10.;
 //         printf("Sux T = %d.%d, H = %d.%d\n", dht11_dat[2], dht11_dat[3], dht11_dat[0], dht11_dat[1]);
 
-         printf("DHT22 Sub:T = %5.2f, H = %5.2f\n", *T, *H);
+//         printf("Sub:T = %5.2f, H = %5.2f\n", *T, *H);
          message1[ 7] = (Temp / 100) % 10 + 48;
          message1[ 8] = (Temp / 10 ) % 10 + 48;
          message1[10] =  Temp        % 10 + 48;
@@ -402,8 +430,8 @@ int DHT_Read(int DhtPin, double *T, double *H, int SensorType)
          message2[10] =  Hum         % 10 + 48;
       }
 
-      printf("%s\n", message1);
-      printf("%s\n", message2);
+      printf("%s %s\n", message1, message2);
+
 
 
       return (0);
